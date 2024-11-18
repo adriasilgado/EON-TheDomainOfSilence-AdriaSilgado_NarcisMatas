001fffe0c941b8931aa361962ff73b31a426b882
adriasilgado <146826970+adriasilgado@users.noreply.github.com>
2024-11-18 16:20:18 +0100

    Lore

--- main.py
+++ main.py
@@ -13,9 +13,21 @@ def play():
         pause(250)
         RecPlay.set_flag(SpriteFlag.INVISIBLE, False)
         pause(250)
-    history()
-def history():
-    print("historia")
+    RecPlay.set_flag(SpriteFlag.INVISIBLE, True)
+    sceneOne()
+def sceneTwo():
+    scene.set_background_image(assets.image("""
+        SceneTwo
+    """))
+    game.show_long_text("Serà EON capaç de retonar la claredat en aquest món? 'Echo' i 'Quietus' ho evitaran a tota costa!",
+        DialogLayout.BOTTOM)
+def sceneOne():
+    scene.set_background_image(assets.image("""
+        SceneOne
+    """))
+    game.show_long_text("Després de la victoria a The Land Of The Forgotten, EON pren camí en una nou aventura, un regne on els germans del silenci 'Echo' i 'Quietus' tenen el control total del mon The Domain Of Silence",
+        DialogLayout.BOTTOM)
+    sceneTwo()
 RecPlay: Sprite = None
 scene.set_background_image(assets.image("""
     myImage
--- main.ts
+++ main.ts
@@ -2,20 +2,40 @@ namespace SpriteKind {
     export const option = SpriteKind.create()
     export const user = SpriteKind.create()
 }
-function play () {
-    RecPlay = sprites.create(assets.image`blink`, SpriteKind.option)
+
+function play() {
+    
+    RecPlay = sprites.create(assets.image`
+        blink
+    `, SpriteKind.option)
     RecPlay.setPosition(99, 76)
-    while (!(controller.A.isPressed())) {
+    while (!controller.A.isPressed()) {
         RecPlay.setFlag(SpriteFlag.Invisible, true)
         pause(250)
         RecPlay.setFlag(SpriteFlag.Invisible, false)
         pause(250)
     }
-    history()
+    RecPlay.setFlag(SpriteFlag.Invisible, true)
+    sceneOne()
 }
-function history () {
-    console.log("historia")
+
+function sceneTwo() {
+    scene.setBackgroundImage(assets.image`
+        SceneTwo
+    `)
+    game.showLongText("Serà EON capaç de retonar la claredat en aquest món? 'Echo' i 'Quietus' ho evitaran a tota costa!", DialogLayout.Bottom)
 }
-let RecPlay: Sprite = null
-scene.setBackgroundImage(assets.image`myImage`)
+
+function sceneOne() {
+    scene.setBackgroundImage(assets.image`
+        SceneOne
+    `)
+    game.showLongText("Després de la victoria a The Land Of The Forgotten, EON pren camí en una nou aventura, un regne on els germans del silenci 'Echo' i 'Quietus' tenen el control total del mon The Domain Of Silence", DialogLayout.Bottom)
+    sceneTwo()
+}
+
+let RecPlay : Sprite = null
+scene.setBackgroundImage(assets.image`
+    myImage
+`)
 play()
